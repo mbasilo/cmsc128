847b413ed7202f664ca54246a95ad32e3e7e482e
Marick <mbasilo@up.edu.ph>
2020-02-22 22:21:53 +0900

    Upload now saves the file locally. Finding ways to display data is under works.
    Added login.html. Currently unaccessible.

--- cmsc128_project/settings.py
+++ cmsc128_project/settings.py
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "upload_temp")
+
+MEDIA_URL = "/upload_temp/"
--- homepage/views.py
+++ homepage/views.py
@@ -3,10 +3,22 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
 
+import csv
 
 def homepage(request):
     return render(request, "homepage/homepage.html")
 
 def upload(request):
+
+	if request.method == "POST":
+		uploaded_file = request.FILES["csv_file"]
+		localFS = FileSystemStorage()
+		localFS.save(uploaded_file.name, uploaded_file)
+
+		data = uploaded_file.read().decode("UTF-8")
+
+		print(data)
+
 	return render(request, "homepage/upload.html", {"title" : "Upload Test"})
